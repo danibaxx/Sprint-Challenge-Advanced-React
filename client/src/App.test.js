c688ff8544736b2b174c5a4c69c88e41196c4175
Danielle B <danibaxx@hotmail.com>
2019-10-13 19:57:24 -0600

    tried to download dependencies I thought I needed to run tests, broke my app

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,3 +7,11 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// afterEach(rtl.cleanup);
+
+// test('Render the heading', () => {
+//   const wrapper = rtl.render(<App />);
+//   const element = wrapper.getByText(/womens world cup players /i);
+//   expect(element).toBeVisible();
+// })
